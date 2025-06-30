8e05a9bf8f349bb7416fe1c1c1aa9f8c78cca361
Ahmad Alfian Nizami <ahmad.nizami@ambersof.com>
2025-06-30 12:27:36 +0700

    nota

--- application/controllers/Penjualan.php
+++ application/controllers/Penjualan.php
@@ -839,6 +839,8 @@ class Penjualan extends CI_Controller {
 		left join tb_brg_keluar tbk on tp.id_keluar = tbk.id_keluar
 		left join tb_brg_masuk tbm on tbk.id_masuk = tbm.id_masuk
 		left join tb_barang tbb on tbb.id_brg  = tbm.id_brg where kode_penjualan = '".$id."'")->result_array();
+		$data['toko'] = $this->db->query("select * from tb_toko where id_toko = '".admin()->id_toko."'")->row();
+		// print_r($data['toko']);die;
 		
 		$this->load->view('nota', $data);
 	}
